9d0c21cba6b095acd017af9912e19061838f7dc4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-02-29 16:21:58 +0100

    minor changes

--- notebooks/gaussian_quadrature.jl
+++ notebooks/gaussian_quadrature.jl
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -29,4 +30,42 @@
 # \int_a^b g(x) dx = \sum_{j=0}^{N-1} v_j g(x_j)
 # $$
 
+# +
+using FastGaussQuadrature
 
+a , b = - 2, 2
+x, w = gausslegendre(4)
+xr = 0.5 * (b-a)
+xm = 0.5 * (b+a)
+xm .+ xr .* x
+
+# +
+"""
+Returns the integral of the function f between a and b, by ten-point Gauss- Legendre integration: 
+the function is evaluated exactly ten times at interior points in the range of integration.
+"""
+function qgauss(f :: Function , a, b)
+    @assert b > a
+    n = 100
+    x, w = gausslegendre(n)
+    xr = 0.5 * (b-a)
+    xm = 0.5 * (b+a)
+    xr * sum( w[i] * f(xm + xr * x[i]) for i in 1:n )
+    
+end
+# -
+
+f(x) = exp( - x^2/2)
+qgauss( f, -10, 10)
+
+sqrt(2π)
+
+using LinearAlgebra
+x, w = gausslegendre(10)
+dot( w,(x.^2))
+
+# $$
+# \int_{-\infty}^\infty e^{-x^2} f(x) dx = \sum^{N-1}_{j=0} w_jf(x_j)
+# $$
+
+gausshermite(10)
